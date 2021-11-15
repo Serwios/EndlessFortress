2300315f32766bfc6556cb98dd24b8f927ec295e
Serwio YK <kucelas@ukr.net>
2021-11-15 17:11:57 +0200

    Platform destructor

---
+++ Assets/Scripts/PlatformDestroyer.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformDestroyer : MonoBehaviour {
+	public GameObject platformDestructionPoint;
+
+    void Start() {
+		platformDestructionPoint = GameObject.Find("PlatformDestructionPoint");        
+    }
+
+    void Update() {
+        if(transform.position.x < platformDestructionPoint.transform.position.x) {
+        	Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/PlatformerGenerator.cs
+++ Assets/Scripts/PlatformerGenerator.cs
@@ -9,16 +9,22 @@ public class PlatformerGenerator : MonoBehaviour {
 
 	private float platformWidth;
 
+	private float randomYKoef;
+	private System.Random random = new System.Random(); 
+
+
     void Start() {
         platformWidth = thePlatform.GetComponent<BoxCollider2D>().size.x;
     }
 
     void Update() {
+    	randomYKoef = (float) random.NextDouble();
+
     	//When generator is above generatorpoint(Camera point) by x
     	if(transform.position.x < generationPoint.position.x) {
     		transform.position = new Vector3(
-    			transform.position.x + platformWidth + distanceBetween, 
-    			transform.position.y, 
+    			(transform.position.x + platformWidth + distanceBetween), 
+    			transform.position.y * randomYKoef, 
     			transform.position.z);
 
     		Instantiate (thePlatform, transform.position, transform.rotation);
